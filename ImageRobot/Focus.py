785f58604b2689062445e6d2f4f890f0c4d728c8
Atyors <thieuw.romain@gmail.com>
2020-01-12 11:10:03 +0100

    [Feature] Focus rework and add functionalities.

--- ImageRobot/Focus.py
+++ ImageRobot/Focus.py
@@ -1,36 +1,113 @@
-import win32gui
+import pygetwindow as gw
 import re
 
 
 class Focus(object):
-    '''
-        This class has been made to implement window management in order to facilitate
+    ''' This class has been made to implement window management in order to facilitate
         image recognition or switch windows if necessary.
     '''
 
 
-    def __init__ (self):
-        ''' TODO
+    def __init__(self):
+        ''' Set the initial values once the class is called.
         '''
-        
-        self._handle = None
+        self.handle = None
 
 
-    def set_foreground_window_containing(self, name):
-        ''' TODO
-        '''
+    def focus_window_containing(self, name):
+        ''' Set the found window in the foreground.
 
-        self._handle = None
-        name_with_wildcard = ".*" + str(name) + ".*"
-        win32gui.EnumWindows(self._window_enum_callback, name_with_wildcard)
-        if self._handle == None:
-            raise Exception("No window containing the text \"" + name + "\" has been found.")
-        win32gui.SetForegroundWindow(self._handle)
-
-
-    def _window_enum_callback(self, hwnd, wildcard):
-        ''' TODO
+            Parameters
+            ----------
+            name: str
+                The name or a part of the name of the searched window.
+            
+            Raises
+            ------
+            Exception
+                If not any window containing the name or part of the name given has been found.
         '''
 
-        if re.match(wildcard, str(win32gui.GetWindowText(hwnd))) is not None:
-            self._handle = hwnd
+        found = False
+
+        for title in gw.getAllTitles():
+            if name in title:
+                self.handle = gw.getWindowsWithTitle(title)[0]
+                self.handle.activate()
+                found = True
+                break
+
+        if not found:
+            raise Exception("Window containing name \"" + str(name) + "\" has not been found.")
+
+
+    def focus_window_with_exact_name(self, name):
+        ''' Set the found window in the foreground.
+
+            Parameters
+            ----------
+            name: str
+                The exact name of the searched window.
+            
+            Raises
+            ------
+            Exception
+                If not any window with the name given has been found.
+        '''
+
+        found = False
+
+        for title in gw.getAllTitles():
+            if name == title:
+                self.handle = gw.getWindowsWithTitle(title)[0]
+                self.handle.activate()
+                found = True
+                break
+
+        if not found:
+            raise Exception("Window with exact name \"" + str(name) + "\" has not been found.")
+
+
+    def maximize_window(self):
+        ''' Maximize the window focused.
+
+            Raises
+            ------
+            Exception
+                If not any window has been focused earlier.
+        '''
+
+        try:
+            self.handle.maximize()
+        except AttributeError:
+            raise Exception("No handle has been focused yet. Use one of the focus functionality first.")
+
+    
+    def minimize_window(self):
+        ''' Minimize the window focused.
+
+            Raises
+            ------
+            Exception
+                If not any window has been focused earlier.
+        '''
+
+        try:
+            self.handle.minimize()
+        except AttributeError:
+            raise Exception("No handle has been focused yet. Use one of the focus functionality first.")
+
+    
+    def restore_window(self):
+        ''' Restore the window focused.
+
+            Raises
+            ------
+            Exception
+                If not any window has been focused earlier.
+        '''
+
+        try:
+            self.handle.restore()
+        except AttributeError:
+            raise Exception("No handle has been focused yet. Use one of the focus functionality first.")
